bfa846a56b4f1dfcf607153bda7262d04de3e6a1
sabin khatri <khatrisabin611@gmail.com>
2026-06-07 21:29:52 +0545

    updated code

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'   
+import { BrowserRouter } from 'react-router-dom'
+import { AppContextProvider } from './context/AppContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>          
-      <App />
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
